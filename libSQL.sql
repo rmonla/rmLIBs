c711c46806df74eeb740cf352a717ac696492fdc
Ricardo N. MONLA <rmonla@gmail.com>
2019-02-27 01:11:17 -0300

    Update libSQL.sql

--- libSQL.sql
+++ libSQL.sql
@@ -3,3 +3,4 @@ sudo mysql -u root -p
 CREATE USER 'rmonla'@'localhost' IDENTIFIED BY 'password';
 
 GRANT ALL PRIVILEGES ON * . * TO 'rmonla'@'localhost';
+GRANT ALL PRIVILEGES ON rmUTNSaa . * TO 'rmUTNSaa'@'localhost';
