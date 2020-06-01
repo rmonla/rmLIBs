10b77e8e90826d9f994f73d1592ec6e21c17e347
Ricardo MONLA <rmonla@gmail.com>
2020-06-01 11:52:10 -0300

    Create ffmpeg.sh

---
+++ cmd/ffmpeg.sh
@@ -0,0 +1,3 @@
+ffmpeg -safe 0 -f concat -i <(find . -type f -name 'TSME_08-05-2020_Parte*' -printf "file '$PWD/%p'\n" | sort) -c copy output.mkv
+
+(find . -type f -name 'TSME_08-05-2020_Parte*' -printf "file '$PWD/%p'\n" | sort) | ffmpeg -protocol_whitelist file,pipe -f concat -safe 0 -i pipe: -vcodec copy -acodec copy "result2.mp4"
