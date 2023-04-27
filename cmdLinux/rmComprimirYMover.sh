40be70c2c25a179cf760365a1cf823cec602c359
Ricardo MONLA <rmonla@gmail.com>
2023-04-27 20:29:24 -0300

    Agrega el autor.

--- cmdLinux/rmComprimirYMover.sh
+++ cmdLinux/rmComprimirYMover.sh
@@ -1,58 +1,56 @@
 #!/bin/bash
+# Autor: Lic. Ricardo MONLA rmonla@gmail.com
+# Uso: ./script.sh -a archivo -d carpeta_destino
+#      ./script.sh -h para mostrar la ayuda
 
-# Función para mostrar el uso del script
-function usage {
-    echo "Uso: $0 -a <archivo a comprimir> -d <carpeta de destino>"
-    echo "Opciones:"
-    echo "  -a <archivo a comprimir>: especifica el archivo a comprimir"
-    echo "  -d <carpeta de destino>: especifica la carpeta de destino"
-    echo "  -h: muestra el uso del script"
+# Función de ayuda
+mostrar_ayuda() {
+  echo "Forma de uso: ./script.sh -a archivo -d carpeta_destino"
+  echo "Opciones:"
+  echo "  -a  Especifica el archivo a comprimir."
+  echo "  -d  Especifica la carpeta de destino para el archivo comprimido."
+  echo "  -h  Muestra esta ayuda."
+  exit 0
 }
 
-# Verificación de que se proporcionaron los argumentos correctos
-while getopts ":a:d:h" opt; do
-    case $opt in
-        a) archivo="$OPTARG"
-            ;;
-        d) carpeta_destino="$OPTARG"
-            ;;
-        h) usage
-            exit 0
-            ;;
-        \?) echo "Opción inválida: -$OPTARG" >&2
-            usage
-            exit 1
-            ;;
-        :) echo "Opción -$OPTARG requiere un argumento." >&2
-            usage
-            exit 1
-            ;;
-    esac
+# Verificación de argumentos
+while getopts "a:d:h" opt; do
+  case ${opt} in
+    a)
+      archivo=$OPTARG
+      if [[ ! -f "$archivo" ]]; then
+        echo "Error: El archivo $archivo no existe."
+        exit 1
+      fi
+      ;;
+    d)
+      carpeta_destino=$OPTARG
+      if [[ ! -d "$carpeta_destino" ]]; then
+        echo "Error: La carpeta destino $carpeta_destino no existe."
+        exit 1
+      fi
+      ;;
+    h)
+      mostrar_ayuda
+      ;;
+    \?)
+      echo "Error: Opción inválida -$OPTARG" 1>&2
+      exit 1
+      ;;
+    :)
+      echo "Error: Opción -$OPTARG requiere un argumento." 1>&2
+      exit 1
+      ;;
+  esac
 done
 
-# Verificación de que se proporcionaron los argumentos obligatorios
-if [ -z "$archivo" ]; then
-    echo "Error: Se debe proporcionar el archivo a comprimir."
-    usage
-    exit 1
+# Verificación de que se especificaron los argumentos requeridos
+if [[ -z "$archivo" || -z "$carpeta_destino" ]]; then
+  echo "Error: Debe especificar el archivo y la carpeta destino."
+  exit 1
 fi
 
-if [ -z "$carpeta_destino" ]; then
-    echo "Error: Se debe proporcionar la carpeta de destino."
-    usage
-    exit 1
-fi
+# Compresión del archivo a .tar.gz
+tar -czvf "$carpeta_destino/$archivo.tar.gz" "$archivo" && rm "$archivo"
 
-# Verificación de que el archivo y la carpeta de destino existen
-if [ ! -f "$archivo" ]; then
-    echo "Error: El archivo $archivo no existe."
-    exit 1
-fi
-
-if [ ! -d "$carpeta_destino" ]; then
-    echo "Error: La carpeta de destino $carpeta_destino no existe."
-    exit 1
-fi
-
-# Comprimir el archivo y moverlo a la carpeta de destino
-tar -czvf "$carpeta_destino/$(basename "$archivo").tar.gz" "$archivo" && rm "$archivo"
+echo "El archivo $archivo se comprimió y se guardó en $carpeta_destino."
