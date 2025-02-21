2debf20d92dea4c9dfa17e8cfbf9f2994471630d
Ricardo MONLA <rmonla@gmail.com>
2025-02-21 19:41:15 -0300

    rmCambiaNomHost.sh - v250221-1940

--- _rmServers/rmCambiaNomHost.sh
+++ _rmServers/rmCambiaNomHost.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Script para cambiar el nombre del host y reiniciar el sistema
 # Ricardo Monla (https://github.com/rmonla)
-# rmCambiaNomHost.sh - v250221-1922
+# rmCambiaNomHost.sh - v250221-1940
 
 clear
 
 # Mostrar el nombre del host actual
-H_ACTUAL=\$(hostname)
-echo "Nombre del host actual: \$H_ACTUAL"
+H_ACTUAL=$(hostname)
+echo "Nombre del host actual: $H_ACTUAL"
 
 # Solicitar el nuevo nombre de host
 read -p "Ingresa el nuevo nombre de host: " H_NUEVO
 
 # Validar que se ingresó un nombre
-if [ -z "\$H_NUEVO" ]; then
+if [ -z "$H_NUEVO" ]; then
   echo "Error: No ingresaste un nombre de host válido."
   exit 1
 fi
 
 # Cambiar el nombre del host en los archivos de configuración y reiniciar
-sudo sed -i "s/\$H_ACTUAL/\$H_NUEVO/g" /etc/hosts /etc/hostname && sudo reboot
+sudo sed -i "s/$H_ACTUAL/$H_NUEVO/g" /etc/hosts /etc/hostname && sudo reboot
